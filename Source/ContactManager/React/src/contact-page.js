eb1b275a4b4320935bfc3021bcbc97c15c623f63
fsolanet5811 <fsolanet5811@gmail.com>
2020-02-04 20:48:22 -0500

    Added contact page

--- Source/ContactManager/React/src/contact-page.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import ContactPageApp from "./components/contact-page/ContactPageApp.js"
-
-ReactDOM.render(<ContactPageApp />, document.getElementById('contact-page'))
