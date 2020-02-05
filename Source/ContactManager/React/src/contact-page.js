a23dcfdfa10b484e6153f1be6758b955d09497f3
RobertForristall <59899671+RobertForristall@users.noreply.github.com>
2020-02-04 20:19:02 -0500

    Fixed contact page app syntax

--- Source/ContactManager/React/src/contact-page.js
+++ Source/ContactManager/React/src/contact-page.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import ContactPage from "./ContactPageApp"
+import ContactPageApp from "./components/contact-page/ContactPageApp.js"
 
 ReactDOM.render(<ContactPageApp />, document.getElementById('contact-page'))
