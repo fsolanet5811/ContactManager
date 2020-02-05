6a3e25e64aef5c43154cfc505745583db22aabea
RobertForristall <59899671+RobertForristall@users.noreply.github.com>
2020-02-04 20:04:15 -0500

    Added Contact Page Files

---
+++ Source/ContactManager/React/src/contact-page.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ContactPage from "./ContactPageApp"
+
+ReactDOM.render(<ContactPageApp />, document.getElementById('contact-page'))
